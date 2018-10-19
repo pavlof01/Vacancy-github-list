6f3a84054c009b482c5c6f23bd85085b34b22ced
DESKTOP-194J07T\Alex <apavlov@icerockdev.com>
2018-10-19 21:16:29 +0700

    setting-up ended /  first start

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src/App';
 import { name as app } from './app.json';
 
 AppRegistry.registerComponent(app, () => App);
