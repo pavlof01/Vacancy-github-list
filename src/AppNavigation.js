a2143737c92e79fe22ef5d90ab0a2aa154e75787
DESKTOP-194J07T\Alex <apavlov@icerockdev.com>
2018-10-23 18:36:33 +0700

    change api and add sagas for fetch posts

--- src/AppNavigation.js
+++ src/AppNavigation.js
@@ -1,6 +1,23 @@
-import { createStackNavigator } from 'react-navigation';
-import Main from './screens';
-import Job from './screens/job';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import { createBottomBarOptions } from './components/bottomBar';
+import Jobs from './screens/jobsScreen';
+import Job from './screens/jobDetail';
+import stackOverflow from './screens/stackOverflow';
+
+const Main = createBottomTabNavigator({
+  Jobs: {
+    screen: Jobs,
+    navigationOptions: createBottomBarOptions('Jobs'),
+  },
+  stackOverflow: {
+    screen: stackOverflow,
+    navigationOptions: createBottomBarOptions('StackOverflow'),
+  },
+  Profile: {
+    screen: Jobs,
+    navigationOptions: createBottomBarOptions('Jobs'),
+  },  
+});
 
 const AppNavigator = createStackNavigator(
   {
